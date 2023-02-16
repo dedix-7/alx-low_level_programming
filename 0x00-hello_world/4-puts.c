9c111bfecd67e13ff86884a84c59a7680cd117bb
Enocka <anzimboingosi@gmail.com>
2023-02-16 14:00:58 -0800

    answer the next question

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - start of execution
  *
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
