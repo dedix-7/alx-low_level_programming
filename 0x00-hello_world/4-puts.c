dfe976997901d93dccdb06d1a90f95af01385c4e
Enocka <anzimboingosi@gmail.com>
2023-02-16 13:39:59 -0800

    answer the next question

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- *
- * main- start of execution
+ * main - start of execution
  *
  * this program uses puts instead of printf
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
