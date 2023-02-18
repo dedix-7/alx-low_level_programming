cc1b79fe45c5e47290ef22753713601f773ac901
Enocka <anzimboingosi@gmail.com>
2023-02-17 17:01:43 -0800

    start the project

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
 /**
  * main - start execution of the program
  * 
@@ -12,7 +11,7 @@ int main(void)
 	int n,x;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n=rand() - RAND_MAX / 2;
 	x=n%10;
 	if (x > 5)
 	{
