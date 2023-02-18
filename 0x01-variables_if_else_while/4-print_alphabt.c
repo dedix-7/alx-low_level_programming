ab17d0fbc3c2568b368acd5fbc77c10aa925827c
Enocka <anzimboingosi@gmail.com>
2023-02-17 23:07:14 -0800

    start the project

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - start printing lowercase characters
+ *
+ * Return: (0) an integer
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if ('a' != 'q' || 'a' != 'e')
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
