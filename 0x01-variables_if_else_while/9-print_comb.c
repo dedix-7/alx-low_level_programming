c044b082fb9b6f436e048e9ad7540edd3bd69243
Enocka <anzimboingosi@gmail.com>
2023-02-18 00:03:54 -0800

    start the project

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - start printing lowercase characters
+ *
+ * Return: (0) an integer
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		putchar(ch);
+		if (ch !57);
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	printf("\n");
+	return (0);
+}
