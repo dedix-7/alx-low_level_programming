ac0f5bc4ba1ebc39f68505ee2778a5d4f1ba3515
Enocka <anzimboingosi@gmail.com>
2023-02-18 01:06:34 -0800

    add 103 file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - start printing lowercase characters
+ *
+ * Return: (0) an integer
+ */
+int main(void)
+{
+
+	int ch1, ch2;
+
+	ch1 = 48;
+	ch2 = 48;
+
+	while (ch2 < 58)
+	{
+		ch1 = 48;
+		while (ch2 < 58)
+		{
+			if (ch2 != ch1 && ch2 < ch1)
+			{
+				putchar(ch2);
+				putchar(ch1);
+				if (ch1 == 57 && ch2 == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			ch1++;
+		}
+		ch2++;
+	}
+	putchar('\n');
+	return (0);
+}
