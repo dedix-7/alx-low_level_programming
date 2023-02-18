5487fffd51a67e0ce4509e5c19a15ca3be225a45
Enocka <anzimboingosi@gmail.com>
2023-02-17 23:01:33 -0800

    start the project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - start printing lowercase characters
+ *
+ * Return: (0) an integer 
+ */
+
+int main(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0)
+}
