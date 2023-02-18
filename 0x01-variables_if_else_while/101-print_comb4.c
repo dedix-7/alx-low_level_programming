33de1a10695fc4d0ff6437b89e4da921b4fb472e
Enocka <anzimboingosi@gmail.com>
2023-02-18 01:27:05 -0800

    change the 101 file to reflect output

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,38 +6,37 @@
  */
 int main(void)
 {
+		int i, e, g;
 
-	int ch1, ch2, ch3;
+	i = 48;
+	e = 48;
+	g = 48;
 
-	ch1 = 48;
-	ch2 = 48;
-	ch3 = 48;
-
-	while (ch2 < 58)
+	while (e < 58)
 	{
-		ch1 = 48;
-		while (ch1 < 58)
+		i = 48;
+		while (i < 58)
 		{
-			ch3 = 58;
-			while (ch3 < 58)
+			g = 48;
+			while (g < 58)
 			{
-				if (ch2 != ch1 && ch2 != ch3 && ch1 != ch3 && ch2 < ch1 && ch1 < ch3)
-			{
-				putchar(ch2);
-				putchar(ch1);
-				putchar(ch3);
-				if (ch1 == 56 && ch2 == 55 && ch3 == 57)
+				if (e != i && e != g && i != g && e < i && i < g)
 				{
-					break;
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 56 && e == 55 && g == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
 				}
-				putchar(',');
-				putchar(' ');
+				g++;
 			}
-			ch3++;
-			}
-		ch1++;
+			i++;
 		}
-		ch2++;
+		e++;
 	}
 	putchar('\n');
 	return (0);
