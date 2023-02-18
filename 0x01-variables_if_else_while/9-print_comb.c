d15586d05115b4dacfa2e3d45ecd6ffa98c47d6f
Enocka <anzimboingosi@gmail.com>
2023-02-18 00:07:03 -0800

    start the project

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 	for (ch = 48; ch < 58; ch++)
 	{
 		putchar(ch);
-		if (ch !=57);
+		if (ch !=57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
