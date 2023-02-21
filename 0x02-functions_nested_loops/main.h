d2182e0f801b83fe227907aaf980eabb54279323
Enocka <anzimboingosi@gmail.com>
2023-02-21 05:59:09 -0800

    add the main header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * header file to include with functions to be called in this project.
+ * Return : void as it just prints for this case
+ * main - is not included as this is just a header file
+ */
+void write(void)
+{
+  putchar('_putchar.c\n');
+}
