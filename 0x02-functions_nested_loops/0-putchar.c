1bf3c31422c90d3866ee57a276871cdf968ab2fd
Enocka <anzimboingosi@gmail.com>
2023-02-21 11:47:06 -0800

    remove the loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,13 @@
 
 int main(void)
 {
-  int display[]={_,p,u,t,c,h,a,r};
-
-  int i;
-
-  for (i=0; i < display[]; i++)
-    _putchar('i');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
   _putchar('\n');
   return (0);
   
