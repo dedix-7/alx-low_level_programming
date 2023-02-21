02ff61a8c6d29cd79bdd6fdb0c83b645657e8c7b
Enocka <anzimboingosi@gmail.com>
2023-02-21 05:34:50 -0800

    add the README and putchar file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * the directive to include the main header file
+ * main - the function to begin execution
+ * Return: type int to ensure success
+ */
+
+int main()
+{
+  write(_putchar.c);
+  return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+#!/bin/bash
+Tis project is about using functions and nested loops.
+The lack of a header file and restrictions againts using the standard library will ensure I can create and use header files
