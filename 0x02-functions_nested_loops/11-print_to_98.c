fbae5557daae0ae9a5646712506cccfc495c3775
Enocka <anzimboingosi@gmail.com>
2023-02-22 05:39:36 -0800

    remove the comma after %d in the printf function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
-  * Description: to print numbers till it gets to 98
+  * Description: to print numbers till it get
   * Return: no value as it prints
-  * print_to_98 - function to print to 98
+  * print_to_98 - function to pnt out numbersr
   * @n: integer to set counter
   */
 
@@ -10,14 +10,14 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
 	}
 	else if (n > 98)
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
