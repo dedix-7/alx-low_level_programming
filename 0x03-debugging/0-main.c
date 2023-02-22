376072ab961306f6af7aa2772faf752bb155b089
Enocka <anzimboingosi@gmail.com>
2023-02-22 12:05:04 -0800

    Add README and first question

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - tests positivity of integer
+ * Return: 0
+ */ 
+int main(void)
+{
+	positive_or_negative(0);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+This project is to test debugging.
+I hope i do well so I recoiver the lost marks this week
