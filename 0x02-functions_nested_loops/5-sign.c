8a92f9716405b55661ed84679d731e3a1bf1e0a6
Enocka <anzimboingosi@gmail.com>
2023-02-21 18:19:47 -0800

    dure to time just check the code

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - checks if its character is in teh alphabet
+ *
+ * @n variable tah reprsents the letter
+ * Return: 0 success, 1 otherwise
+ */
+int print_sign(int n); 
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0);
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
