f67c99a076b8abbbbb28503332acccffe9062fca
Enocka <anzimboingosi@gmail.com>
2023-02-22 04:50:05 -0800

    add the summation filee ad

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,6 +3,8 @@
 /**
  * Description: adds two integers and returms their sum
  * add - function from where execution begins
+ * @a: first number to be added
+ * @b: second function to be added
  * Return: an integer sum
  */
 int add(int a,int b)
