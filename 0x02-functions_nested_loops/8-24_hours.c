6fa2f0ec1bceaa3b52c1b0abb7fcfd9b8e3fa925
Enocka <anzimboingosi@gmail.com>
2023-02-21 18:52:31 -0800

    dure to time just check the code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * jack_bauser - show day minutes
+ * jack_bauer - show day minutes
  * Return: no return type
  */
 void jack_bauer(void)
 {
-	int i,j,k,l;
+	int i, j, k, l;
 
-	for (i =48; i <= 50; i++)
+	for (i = 48; i <= 50; i++)
 	{
 		for (j = 48; j <= 57; j++)
 		{
 			for (k = 48; k <= 53; k++)
 			{
-				for (l = 48; l <=57; l++)
+				for (l = 48; l <= 57; l++)
 				{
-					if (i >=50 && j >= 52)
+					if (i >= 50 && j >= 52)
 						break;
 					_putchar(i);
 					_putchar(j);
