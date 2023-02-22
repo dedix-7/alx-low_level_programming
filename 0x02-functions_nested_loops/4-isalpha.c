80adfdc750ae34f15fb959b0c26f221c0a09e031
Enocka <anzimboingosi@gmail.com>
2023-02-21 18:09:08 -0800

    question 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks if its character is in teh alphabet
+ *
+ * @c: variable tah reprsents the letter
+ * Return: 0 success, 1 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122 ) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
