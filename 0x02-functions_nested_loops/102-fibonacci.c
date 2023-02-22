7563b78c73f43e1b047298dc8195466fb3e175da
Enocka <anzimboingosi@gmail.com>
2023-02-22 08:41:19 -0800

    Betty comments

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,7 @@
 /**
  * main - function to print 50 fibbonnacci number
  *
- * Return - int of 0 for success
+ * Return: int of 0 for success
  *
  */
 
@@ -10,20 +10,23 @@ int main(void)
 {
 	int counter1, variable1, counter2;
 
-	counter1 = 0; variable1 = 0, counter2 = 0;
+	counter1 = 0;
+	variable1 = 0;
+	counter2 = 0;
 
-	for (counter1 = 0; counter1 <=50; counter1++)
+	for (counter1 = 0; counter1 <= 50; counter1++)
 	{
 		if (variable1 == 0)
-			printf("%d, ",variable1);
+			printf("%d, " , variable1);
 		else if (variable1 == 1)
-			printf("%d, ",variable1);
-
-		counter2 += variable1;
-
-		printf ("%d ,", counter2);
+			printf("%d, " , variable1);
+		else
+		{
+			counter2 += variable1;
+			
+			printf ("%d ," , counter2);
+		}
 	}
-	
 	return (0);
 }
 
