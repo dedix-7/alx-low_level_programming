34764849bc1cef924ccc758aa704f1d3340b7e32
Enocka <anzimboingosi@gmail.com>
2023-02-21 17:53:28 -0800

    answer question 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  _islower - checks for lowercase
+ *
+ *  Return: an integer, 0 success 1 failure
+ */
+int _islower(int c)
+{
+	if (c >=97 && c <= 122)
+		return (1);
+	else
+		return(0);
+}
