928f7cb00c819728826f0a2b3b0cb2a3d92cff78
Enocka <anzimboingosi@gmail.com>
2023-02-22 08:12:08 -0800

    initial commit for the project

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_times_table - Prints the n times table
+ *
+ * @arguments: nThere are no arguments
+ *
+ * Return: no return
+ */
+
+void main(void)
+{
+	int sum, i;
+
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf ( "%d\n", sum);
+}
