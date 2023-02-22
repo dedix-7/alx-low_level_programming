68f1a9d74337c061d6b9d858a2885aa133fee433
Enocka <anzimboingosi@gmail.com>
2023-02-22 10:06:07 -0800

    use variables in fbbonnacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-	int counter1, variable1, counter2, variable2;
+	long int counter1, variable1, counter2, variable2;
 
-	counter1 = 0;
 	variable1 = 0;
 	variable2 = 1;
 
-	for (counter1 = 0; counter1 <= 50; counter1++)
+	printf("%d ,%d ,");
+
+	for (counter1 = 0; counter1 <= 48; counter1++)
 	{
 	counter2 = variable1 + variable2;
 	printf("%d, ", counter2);
