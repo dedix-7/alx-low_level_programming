f05fddd89e13323ee7241394585870a349a8e83e
Enocka <anzimboingosi@gmail.com>
2023-02-22 05:17:29 -0800

    add the 11 file to print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ *
+ * Return: no value as it prints
+ * print_to_98 - function to print to 98
+ * @n: integer to set counter
+ * Description: a function that prints numbers to 98
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{ 
+		for (; n <= 98; n++)
+		{
+			printf("%d ," n);
+		}
+	}
+	if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d ," n);
+		}
+	}
+}
