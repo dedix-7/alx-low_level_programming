8ac8e274b59bf57f1a8383be195a6caf72226c36
Enocka <anzimboingosi@gmail.com>
2023-02-21 16:32:54 -0800

    remove spaces at the beginning of line snad whitespace after the last two lines

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include "main.h"
 /**
- * the directive to include the main header file
- * main - the function to begin execution
+ * This fnction prints ut individual characters
+ *
+ *  the directive to include the main header file
+ *
+ *  main(voi) - the function to begin execution
  * Return: type int to ensure success
  */
 
 int main(void)
 {
-   _putchar('_');
-   _putchar('p');
-   _putchar('u');
-   _putchar('t');
-   _putchar('c');
-   _putchar('h');
-   _putchar('a');
-   _putchar('r');
-   _putchar('\n');
-   return (0);  
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);  
 }
