d6c88691f54d78751b1cf2c8aa182b876ecbc7cf
enox <anzimboingosi@gmail.com>
2023-06-07 05:28:32 -0700

    add a print rverse function

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string of characters n a newline
+ * @s: string to be printed
+ *
+ * Return: no return, hence void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+	_puts_recursion(s + 1);
+	_putchar(*s);
+}
