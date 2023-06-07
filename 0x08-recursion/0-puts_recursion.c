ddef9fd8179bf97af32a12e8150eab29c164f394
enox <anzimboingosi@gmail.com>
2023-06-07 04:53:42 -0700

    add the first file for a puits clone

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string of characters to the stdoutput and a newline
+ * @s: string to be printed
+ *
+ * Return: no return, hence void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return (void);
+	}
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,5 @@
+##Recursion
+This series of projects is about recusrion.
+After taking a break, I'll be writing down each code for this project.
+##Authors
+@ingowriter on Twitter
