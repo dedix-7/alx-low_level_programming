c2bdb5ae782166d7ad9209aecb60cc49fab171b5
enox <anzimboingosi@gmail.com>
2023-06-07 10:38:32 -0700

    wrap return function in braces

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strlen_recursion - finds the length of a string
- * @s: string whose ;length is to be found
- * @Return: length of the string
+ * @s: string whose length is to be found
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
@@ -10,9 +10,9 @@ int _strlen_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		return (1);
+		return (0);
 	}
 	length++;
 	length += _strlen_recursion(s + 1);
-	return length;
+	return (length);
 }
