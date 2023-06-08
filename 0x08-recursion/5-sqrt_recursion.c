f0e55810d9a61ef615c7556bbdbde8b3d39a978e
enox <anzimboingosi@gmail.com>
2023-06-08 01:16:55 -0700

    fi root in one function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,2 +1,33 @@
 #include "main.h"
+/**
+ * squareroot - find the squareroot
+ * @n: number to find root of
+ * @i: iteratto recur
+ * Return:  squareroot
+ */
+int squareroot(int n, int i)
+{
+	int root;
 
+	if ((i * i) == n)
+	{
+		root = i;
+		return (root);
+	}
+	else if (((i * i) > n) || (n < 0))
+	{
+		root = -1;
+		return (root);
+	}
+	root = squareroot(n, (i + 1));
+	return (root);
+}
+/**
+ * _sqrt_recursion - find sqaureroot
+ * @n: number to find root
+ * Return: root of the number
+ */
+int _sqrt_recursion(int n)
+{
+	return (squareroot(n, 0));
+}
