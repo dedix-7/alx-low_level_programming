86bb6a5c510ddf414119e9096b25a9a05200b61b
enox <anzimboingosi@gmail.com>
2023-06-08 03:08:52 -0700

    use equality to find prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,22 +3,28 @@
  * checkprime - function that checks if a number is prime
  * @n: number to be checked
  * @i: iterator
+ * Return: 0 or 1 for prime
  */
-int checkprime(int n,int i)
+int checkprime(int n, int i)
 {
 	int check;
 
-	if ((n <= 1) || (((n % i) == 0) && (i != n)))
+	if (n <= 1)
 	{
 		check = 0;
 		return (check);
 	}
-	else if ((n % i) == 0)
+	else if (((n % i) == 0) && (n == i))
 	{
 		check = 1;
 		return (check);
 	}
-	check = checkprime(n,i++);
+	else if (((n % i) == 0) && (i < n))
+	{
+		check = 0;
+		return (check);
+	}
+	check = checkprime(n, i++);
 	return (check);
 }
 /**
