c2b11f06404926547a75f7174c6e92fb533379d5
enox <anzimboingosi@gmail.com>
2023-06-08 01:55:44 -0700

    Add numbers to make prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * checkprime - function that checks if a number is prime
+ * @n: number to be checked
+ * @i: iterator
+ */
+int checkprime(int n,int i)
+{
+	int check;
+
+	if ((n <= 1) || (((n % i) == 0) && (i != n)))
+	{
+		check = 0;
+		return (check);
+	}
+	else if ((n % i) == 0)
+	{
+		check = 1;
+		return (check);
+	}
+	check = checkprime(n,i++);
+	return (check);
+}
+/**
+ * is_prime_number - returns the value of a prime number
+ * @n: number to be checked
+ * Return: 1 for prinme,0 otherwise\
+ *
+ */
+int is_prime_number(int n)
+{
+	return (checkprime(n, 2);
+}
