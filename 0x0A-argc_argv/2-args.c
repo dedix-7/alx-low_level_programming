23470dcd5779e082529575b46d856bc5d6aca3d3
enox <anzimboingosi@gmail.com>
2023-06-09 07:41:56 -0700

    print given arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - print arguments it recives
+ * @argc: count of arguments
+ * @argv: array of strings to hold arguments
+ * Return: 0 on sucess, nonzero elsewise
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
+	  
