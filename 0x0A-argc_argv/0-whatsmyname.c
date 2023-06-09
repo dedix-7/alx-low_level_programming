b95aa1cce561931a7a1f26737e93c927d078c3f8
enox <anzimboingosi@gmail.com>
2023-06-09 07:09:18 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+/**
+ * main - return the name of the function
+ * @argc: the number of command arguments
+ * @argv: arrayholding the command strings
+ * Return: 0 on success, 1 otherwise
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+So after reading about commandline arguments, this project will showcase my ability to undertsnad and apply command line arguments in C
+##0-whatsmyname.c
+It prints the name of the program. 
