f4e91cc9a162d77c089c451d7388c20bd81ca985
enox <anzimboingosi@gmail.com>
2023-06-18 06:28:40 -0700

    acccount for size 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int fillcounter;
 
 	createdarray = malloc(size);
-	if (createdarray == NULL)
+	if (createdarray == NULL || size == 0)
 	{
 		return (NULL);
 	}
