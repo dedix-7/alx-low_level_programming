0336284c2df3b2df774c628189f8fb8b8e155e98
enox <anzimboingosi@gmail.com>
2023-06-19 17:11:35 -0700

    use arguments instead of hard coding values

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->age = 4;
-	d->name = "sammy";
-	d->owner = "caroline";
+	d->age = age;
+	d->name = name;
+	d->owner = owner;
 }
