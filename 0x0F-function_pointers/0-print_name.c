d980a53a5b4425d2e24435eb5895c369ac968119
enox <anzimboingosi@gmail.com>
2023-06-21 12:19:54 -0700

    print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,5 @@
 #include "function_pointers.h"
 /**
- * print - prints a name
- * @c: string to be printed
- * Return: no return, void
- */
-void print(char *c)
-{
-	int charcount = 0;
-
-	while (c[charcount] != '\0')
-	{
-		_putchar(c[charcount]);
-		charcount++;
-	}
-}
-/**
  * print_name - prints a name
  * @name: name to be printed
  * @f: function ointer to the printing function
