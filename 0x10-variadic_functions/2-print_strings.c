9031ca7ff16e724a46f5295d30674d3314406a30
enox <anzimboingosi@gmail.com>
2023-06-22 17:24:57 -0700

    print strings in place of integers

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints given strings then a new line
+ * @separator: string between numbers
+ * @n: number of passed integers
+ * Return: no return
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int count;
+	char str[];
+	va_list args;
+
+	va_start(args, n);
+	for (count = 0; count < n; count++)
+	{
+		str = va_arg(args, char[]);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}	
+		if (separator != NULL && count < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
