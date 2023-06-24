ea6c19938f41ad6f3e8fdc6928444dfb3e2a8fc5
enox <anzimboingosi@gmail.com>
2023-06-23 17:31:52 -0700

    print argument number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - print number of arguments passed
+ * @argc: argument count
+ * @argv: string argukents
+ * Return: 0 on sucess
+ */
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
