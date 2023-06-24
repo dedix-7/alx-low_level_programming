cd92d10eb6047497c48bb46551ba35d32890047b
enox <anzimboingosi@gmail.com>
2023-06-23 22:03:30 -0700

    test strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcpy - copy a string
+ * @dest: destination string
+ * @src: source address
+ * Return: address to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while (*dest++ = *src++);
+	return (dest);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+# About.
+So i should have done this filebut I deleted the repo
+Am testing if a verstion strcoy works
+## 9-strcpy.c
+The versuon I wnat to test uss this checker
