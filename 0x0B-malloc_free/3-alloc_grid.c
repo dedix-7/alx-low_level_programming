eea635b7355ff340d7cfb1097877c4ce61ba500e
enox <anzimboingosi@gmail.com>
2023-06-25 09:11:32 -0700

    betty code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,12 +11,14 @@ int **alloc_grid(int width, int height)
 
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
-	array = malloc(sizeof (int *) * width);
+	array = malloc(sizeof(int *) * width);
 	if (array == NULL)
 		return (NULL);
 	for (count = 0; count < height; count++)
 	{
 		array[count] = malloc(sizeof(int) * width);
+		if (array[count] == NULL)
+			return (NULL);
 	}
 	for (count = 0; count < height; count++)
 	{
