2ba26926ca8663b916386d9645c051e7be90d086
enox <anzimboingosi@gmail.com>
2023-06-26 09:13:31 -0700

    free allocated grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,17 @@
 #include "main.h"
+/**
+ * free_grid - free a previously allocated 2d array
+ * @grid: double pointer
+ * @height: number of rows
+ * Return: no return
+ */
+void free_grid(int **grid, int height)
+{
+	int rowcount;
+
+	for (rowcount = height; rowcount >= 0; rowcount--)
+	{
+		free(grid[rowcount]);
+	}
+	free(grid);
+}
