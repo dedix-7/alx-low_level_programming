96c27e73fb9e95e2f0ba2cbe1ca06b187bfec714
enox <anzimboingosi@gmail.com>
2023-06-29 23:57:01 -0700

    print number of nodes in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list.
+ * It is assumed the tail points to NULL
+ * @h: head pointer passed
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	const list_t *temporary = h;
+
+	while (temporary != NULL)
+	{
+		count += 1;
+		temporary = temporary->next;
+	}
+	return (count);
+}
