335e0438c54fcd765638f8d30dbd90b6f42fe5f8
enox <anzimboingosi@gmail.com>
2023-06-30 19:03:04 -0700

    add at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,93 +1,38 @@
 #include "lists.h"
-/**
- * _strlen - gets the lengtrh of a stribng
- * @str: string whose length is to be found
- * Return: length iof string or 0 if it's empty
- */
-int _strlen(const char *str)
-{
-	int index = 0, length = 0;
 
-	if (str == NULL)
-	{
-		return (0);
-	}
-	while (str[index] != '\0')
-	{
-		length += 1;
-		index += 1;
-	}
-	return (length);
-}
 /**
- * createnode - createsc a new node of list_t
- * @str: string to be passed to new node
- * Return: address of new node
+ *add_node_end - adds a node at the end of the list
+ *@head: double pointer to the list
+ *@str: sring to add at the end
+ *
+ *Return: address of the new element
  */
-list_t *createnode(const char *str)
-{
-	list_t *result;
 
-	result = malloc(sizeof(list_t));
-	if (result == NULL)
-	{
-		return (NULL);
-	}
-	result->str = strdup(str);
-	result->len = _strlen(str);
-	if (result->str == NULL)
-	{
- 		free(result);
-		return (NULL);
-	}
-	return (result);
-}
-/**
- * findnode - finds a node with a particular next pointer
- * @head: head of the list
- * @nextp: next pointer to be found
- * Return: address of that node, or NULL if it fails
- */
-list_t *findnode(list_t **head)
-{
-	list_t *temp;
-
-	temp = *head;
-	while (temp != NULL)
-	{
-		if (temp->next == NULL)
-			return temp;
-		temp = temp->next;
-	}
-	return (temp);
-}
-/**
- * insertafternode - insert a node after a given node
- * @nodeafter: node to indert afyer
- * @newnode: the new node to be inserted
- * Return: no return
- */
-void insertafternode(list_t *nodeafter, list_t *newnode)
-{
-	newnode->next = nodeafter->next;
-	nodeafter->next = newnode;
-}
-/**
- * add_node_end - function that resets the head of a list
- * @head: head of the list
- * @str: string to be duplicated in the end
- * Return: address of the new element
- */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *addrhead, *newnode;
+	unsigned int len = 0;
+	list_t *newnode, *temp = *head;
 
-	addrhead = createnode(str);
-	if (addrhead == NULL)
-	{
+	while (str[len] != '\0')
+		len++;
+
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
 		return (NULL);
+	newnode->str = strdup(str);
+	newnode->len = len;
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
 	}
-	newnode = findnode(head);
-	insertafternode(newnode, addrhead);
-	return (addrhead);
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = newnode;
+	return (newnode);
 }
