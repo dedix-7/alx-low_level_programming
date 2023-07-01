8c41d83c36730232ab84dfa8c96f23384deb57c0
enox <anzimboingosi@gmail.com>
2023-06-30 17:41:58 -0700

    add new node at the head

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,28 @@
 #include "lists.h"
 /**
+ * createnode - createsc a new node of list_t
+ * @str: string to be passed to new node
+ * Return: address of new node
+ */
+list_t *createnode(const char *str)
+{
+	list_t *result;
+
+	result = malloc(sizeof(list_t));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	result->str = strdup(str);
+	if (result->str == NULL)
+	{
+		free (result);
+		return (NULL);
+	}
+	result->next = NULL;
+	return (result);
+}
+/**
  * add_node - function that resets the head of a list
  * @head: head of the list
  * @str: string to be duplicated in the head
@@ -7,12 +30,14 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *addrhead, newhead;
+	list_t *addrhead;
 
-	newhead.str = strdup(str);
-	newhead.next = &(**head);
-	addrhead = &newhead;
-	head = &addrhead;
-
+	addrhead = createnode(str);
+	if (addrhead == NULL)
+	{
+		return (NULL);
+	}
+	addrhead->next = *head;
+	*head = addrhead;
 	return (addrhead);
 }
