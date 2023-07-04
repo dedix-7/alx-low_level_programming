a5ac6a28cfbcb49615c0bffe5e4a346d3f9eddee
enox <anzimboingosi@gmail.com>
2023-07-04 09:23:17 -0700

    set the head

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * add_nodeint - adds a node at the beginning of the list
+ * @head: current head of the list
+ * @n: int elemnt of the list.
+ * Return: address of new element or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t new;
+	listint_t *newadddr = &new;
+
+	new.next = *head;
+	*head = newadddr;
+
+	return (newadddr);
+}
