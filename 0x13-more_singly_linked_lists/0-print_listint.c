4afc5ef31588a8a7cb52fd2235574d8cdd9916e3
enox <anzimboingosi@gmail.com>
2023-07-03 19:11:01 -0700

    Print all elemts of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - print all elements of a linked list
+ * @h: head of the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t numnodes = 0;
+	listint_t *temp;
+
+	temp = h;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		numnodes++;
+		temp = temp->next;
+	}
+	return (numnodes);
+}
---
+++ 0x13-more_singly_linked_lists/REAME.md
@@ -0,0 +1,9 @@
+# About
+This project looks to keep me practicing my linked list skills
+I managed to reread malloc and pointers during the day since I had difficulties in them
+
+## 0-print_listint.c
+prints the elements of a linked list
+
+# Author
+dedix-7/enox
