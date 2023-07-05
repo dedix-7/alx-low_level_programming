45568553ae5ec668c3e3dd627cb259532d246741
enox <anzimboingosi@gmail.com>
2023-07-04 19:48:55 -0700

    use malloc

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,19 @@
 #include "lists.h"
 /**
+ * newnode - creates a new node
+ * @value: value to put in new node
+ * Return: pointer to new node
+ */
+listint_t *newnode(int value)
+{
+	listint_t *create = malloc(sizeof(listint_t));
+
+	if (!create)
+		return (NULL);
+	create->n = value;
+	return (create);
+}
+/**
  * add_nodeint - adds a node at the beginning of the list
  * @head: current head of the list
  * @n: int elemnt of the list.
@@ -7,12 +21,12 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t new;
-	listint_t *newadddr = &new;
+	listint_t *new;
 
-	new.next = *head;
-	new.n = n;
-	*head = newadddr;
-
-	return (newadddr);
+	new = newnode(n);
+	if (!new)
+		return (NULL);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
