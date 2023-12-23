fd9b357be90517c5b4665d9e2e31ed480745e6b1
enox <anzimboingosi@gmail.com>
2023-12-23 10:16:09 -0800

    use number permissions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -90,13 +90,11 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to, close_to, close_from;
 	ssize_t lenr, lenw;
 	char buffer[1024];
-	mode_t file_perm;
 
 	check97(argc);
 	fd_from = open(argv[1], O_RDONLY);
 	check98((ssize_t)fd_from, argv[1], -1, -1);
-	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	check99((ssize_t)fd_to, argv[2], fd_from, -1);
 	lenr = 1024;
 	while (lenr == 1024)
