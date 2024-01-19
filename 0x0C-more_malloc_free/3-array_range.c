40d9fcdf3c608fa7f902f7f42f47a4cf01d3d404
nox <anzimboingosi@gmail.com>
2024-01-18 18:22:01 -0800

    check for malloc's fail

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,6 +14,8 @@ int *array_range(int min, int max)
 		return (NULL);
 	size = max - min + 1;
 	arr = malloc(sizeof(int) * size);
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++, min++)
 		arr[i] = min;
 	return (arr);
