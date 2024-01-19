3d2cab8fa87dfb673eb657e0064d1506308b859c
nox <anzimboingosi@gmail.com>
2024-01-18 17:58:54 -0800

    use stepwise algorithm

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,59 +1,32 @@
 #include "main.h"
+#include "main.h"
 /**
- * _strlen - get the length of a string
- * @str: string to get length of
- * Return: length od string
- */
-int _strlen(char *str)
-{
-	int i = 0;
-
-	while (str[i])
-		i++;
-	return (i);
-}
-/**
- * _strcpynob - copy withut appending null byte#
- * @src: string to copy
- * @dest: where to copy the string
- * Return: destination address
- */
-char *_strcpynob(char *dest, char *src)
-{
-	int len, i;
-
-	len = _strlen(src);
-	for (i = 0; src[i] && i < len; ++i)
-		dest[i] = src[i];
-	return (dest);
-}
-/**
- * string_nconcat - concatenate tw strings at a new address in memory
- * @s1: first string to concantenate
- * @s2: secodn string
- * concatentaes up to n bytes of secodns tring
- * @n: butes of str2 to cocantenate
- * Return: new address concantenated
+ * string_nconcat - concatenate two strings in a newly allocated space in
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes to contante limit
+ * Return: pointer to newly allocated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, size;
+	unsigned int len1 = 0, len2 = 0, iter = 0;
 	char *new;
 
 	if (!(s1))
 		s1 = "";
 	if (!(s2))
 		s2 = "";
-	size = _strlen(s1) + _strlen(s2) + 1;
-	new = malloc(sizeof(char) * size);
-	if (new == NULL)
+	while (s1[len1])
+		len1++;
+	while (s2[len2] && len2 < n)
+		len2++;
+	new = malloc((len1 + len2 + 1) * sizeof(char));
+	if (!(new))
 		return (NULL);
-	_strcpynob(new, s1);
-	j = _strlen(s1);
-	for (i = 0; (i < n) && (s2[i]); i++)
-		new[j + i] = s2[i];
-	if (s2[i])
-		j++;
-	new[j + i] = '\0';
+	for (iter = 0; iter < len1; iter++)
+		new[iter] = s1[iter];
+	for (iter = 0; iter < len2; iter++)
+		new[len1 + iter] = s2[iter];
+	new[len1 + len2] = '\0';
 	return (new);
 }
