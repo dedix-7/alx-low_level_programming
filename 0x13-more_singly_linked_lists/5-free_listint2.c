8fa48a046b3ef0bfbf0c907ffcc4772c5135c2dd
nox <anzimboingosi@gmail.com>
2024-02-07 02:41:36 -0800

    switch the head as you delete

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 /**
  * free_listint2 - function that frees a list
- * @head:head of the list
+ * @head:address of the head of the list
  * Return: void, it's always successful
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head, *prev = *head;
+	listint_t *prev = *head;
 
 	if (head == NULL)
 		return;
-	while (temp)
+	while (*head)
 	{
-		prev = temp;
-		temp = temp->next;
+		prev = *head;
+		*head = (*head)->next;
 		free(prev);
 	}
-	*head = NULL;
-	head = NULL;
 }
