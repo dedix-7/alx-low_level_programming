f9668fb0f1d4ad5032cc183fb91a2a83dfbfcd64
nox <anzimboingosi@gmail.com>
2024-02-07 15:18:52 -0800

    add at a partocular index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *new, *prev;
-	unsigned int i =0;
+	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
-	if (!(prev))
+	if (i != idx)
 		return (NULL);
 	new->next = temp;
 	prev->next = new;
