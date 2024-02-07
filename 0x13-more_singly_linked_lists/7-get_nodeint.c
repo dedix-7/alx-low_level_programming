909fea4c878bbefe79622d3308cfa883fea7292c
nox <anzimboingosi@gmail.com>
2024-02-07 14:44:42 -0800

    et node at an index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,20 @@
 #include "lists.h"
 /**
  * get_nodeint_at_index - get the node at a particular index
- * @head: 
+ * @head: head of the lists
+ * @index: index of node to return
+ * Return: node at that indx or NULL on failure
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *prev;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0, prev = head; prev && i < index; i++)
+		prev = prev->next;
+	if (!(prev))
+		return (NULL);
+	return (prev);
+}
