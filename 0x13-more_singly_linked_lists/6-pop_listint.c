a435c6a355be19af47cf6a69f8ea4859ecb6a854
nox <anzimboingosi@gmail.com>
2024-02-07 02:58:49 -0800

    delete head node and set it to next

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,19 +1,22 @@
 #include "lists.h"
 /**
  * pop_listint - a function that deletes the head of a linked list
+ * Basically it's setting node two to be the head
  * @head: address o0f the head to the list
  * Return: 0 if it's empty, head's data otherwise
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *prev;
 	int ret;
+	listint_t *prev;
 
 	if (head == NULL)
 		return (0);
 	if (*head == NULL)
 		return (0);
 	ret = (*head)->n;
-	free(*head);
+	prev = *head;
+	*head = (*head)->next;
+	free(prev);
 	return (ret);
 }
