5acc0e9038d9bf938ce50ee61faeaeaac4c60f99
nox <anzimboingosi@gmail.com>
2024-02-07 15:14:53 -0800

    add at a partocular index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - insert a node at a given index
+ * @head: address of the head of the list
+ * @idx: index to insert at
+ * @n: data of the node
+ * Return: node address, or NULL on failure
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *new, *prev;
+	unsigned int i =0;
+
+	if (head == NULL)
+		return (NULL);
+	temp = *head;
+	new = malloc(sizeof(listint_t));
+	new->next = NULL;
+	new->n = n;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (temp && i < idx)
+	{
+		prev = temp;
+		temp = temp->next;
+		i++;
+	}
+	if (!(prev))
+		return (NULL);
+	new->next = temp;
+	prev->next = new;
+	return (new);
+}
