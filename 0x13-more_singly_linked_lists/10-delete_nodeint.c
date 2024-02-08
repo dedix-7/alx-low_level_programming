a0d84c2de3f9af36fd87e3eced0e4e47b0717320
nox <anzimboingosi@gmail.com>
2024-02-07 16:07:00 -0800

    delete at given index, a problem wit two nodes

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - delete a node at a partocular index
+ * @head: address of the head
+ * @index: index to dleet at
+ * Return: 1 on success, -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *prev;
+	unsigned int i = 0;
+
+	if ((head == NULL) || (*head == NULL))
+		return (-1);
+	temp = *head;
+	prev = *head;
+	if (index == 0)
+	{
+		temp = temp->next;
+		*head = temp;
+		free(prev);
+		return (1);
+	}
+	while ((temp) && (i < (index - 1)))
+	{
+		prev = temp;
+		index++;
+		temp = temp->next;
+	}
+	if (!(temp))
+		return (-1);
+	prev = temp->next;
+	temp->next = temp->next->next;
+	free(prev);
+	return (1);
+}
