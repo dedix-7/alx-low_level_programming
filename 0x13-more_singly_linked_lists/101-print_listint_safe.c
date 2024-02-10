bd0289f786a11add30d9d3eb8976463eb521f7fd
nox <anzimboingosi@gmail.com>
2024-02-09 23:11:21 -0800

    print till entry point

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,62 @@
+#include "lists.h"
+/**
+ * find_listint_loop - function that finds
+ * a loop in a linked list
+ * It returns the entry point of the loop
+ * @head: head of the list
+ * Return: address of the beginning node or NULL if there's not
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *fast, *slow;
+
+	if ((head == NULL) || (head->next == NULL))
+		return (NULL);
+	fast = slow = head;
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			fast = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+	return (NULL);
+}
+/**
+ * print_listint_safe - prints a liked lists, even ones with a loop
+ * @head: hread of the list
+ * Return: number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *slow, *entry;
+	size_t i = 0;
+
+	if (!(head))
+		return (0);
+	slow = (listint_t *)head;
+	entry = find_listint_loop(slow);
+	if (slow == entry)
+	{
+		printf("[%p] %d\n", (void *) slow, slow->n);
+		i++;
+		return (i);
+	}
+	while (slow)
+	{
+		printf("[%p] %d\n", (void *) slow, slow->n);
+		i++;
+		if (slow->next == entry)
+			break;
+		slow = slow->next;
+	}
+	return (i);
+}
