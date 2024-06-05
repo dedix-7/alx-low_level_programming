b8ef6d0a87b62d36e86ec9c01661f0b251dedd8c
noxcodes <anzimboingosi@gmail.com>
2024-06-04 18:39:15 -0700

    use recursion alg

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -49,3 +49,8 @@ def island_perimeter(grid):
         perim += defines(i, j - 1)
         perim += defines(i - 1, j)
         return perim
+
+        for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return (defines(i, j))
