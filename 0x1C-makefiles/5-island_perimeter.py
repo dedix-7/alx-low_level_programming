548f29b1a776324eaa321822b40d95285dab58c8
noxcodes <anzimboingosi@gmail.com>
2024-06-04 19:18:23 -0700

    document well

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -31,32 +31,6 @@ def island_perimeter(grid):
         The perimeter of the island as an integer.
      """
 
-#    visit = set()
-
-#    def defines(i, j):
-        """ a depth first function to find the root nodes of te island
-        """
-
-        """"
-        if i >= len(grid) or j >= len(grid[0]) or \
-           i < 0 or j < 0 or grid[i][j] == 0:
-            return 1
-        if (i, j) in visit:
-            return (0)
-
-        visit.add((i, j))
-        perim = defines(i, j + 1)
-        perim += defines(i + 1, j)
-        perim += defines(i, j - 1)
-        perim += defines(i - 1, j)
-        return perim
-
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j]:
-                return (defines(i, j))
-        """
-
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                             if (grid[i][j] == 1):
