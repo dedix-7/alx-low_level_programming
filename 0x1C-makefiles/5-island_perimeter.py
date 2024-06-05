d0c951cc5231eb58be7dfe6161878e43d42e4151
noxcodes <anzimboingosi@gmail.com>
2024-06-04 18:41:57 -0700

    still indent after closure

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -50,7 +50,7 @@ def island_perimeter(grid):
         perim += defines(i - 1, j)
         return perim
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]:
-                    return (defines(i, j))
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return (defines(i, j))
